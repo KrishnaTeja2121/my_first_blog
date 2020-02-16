68c4ee34c1a1d9dd40bc32598519fbc16166e602
ola <ola@example.com>
2020-02-16 21:22:35 +0530

    My Django Girls app, first commit

---
+++ Python_intro.py
@@ -0,0 +1,42 @@
+
+# print('Hello World')
+
+# dict = [1, 5, 8, 9, 45, 158, 265, 74521, 25, 42]
+
+# print(len(dict))
+
+#dict.sort()
+
+#print(dict)
+
+#part =  {'name' : 'ravi', 'age' : '25', 'numbers' : '[1,5,6]'}
+#print(part)
+#part['sex'] = 'male'
+#print(part)
+#part.pop('sex')
+#print(part)
+
+
+#a, b = 45, 26
+
+#print(a*b)
+
+#def hi():
+	#print('Hi There')
+	#print('How are you ?')
+
+#hi() 
+
+
+def hi(name):
+	if name == 'ola':
+		print('Hi ', name)
+	elif name == 'sonja':
+		print('Hi ', name)
+	else:
+		print('Hi anonymous!')
+
+hi('sonja')
+
+
+
